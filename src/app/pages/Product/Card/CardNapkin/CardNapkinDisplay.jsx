dea89aa25be55e80c42f22dc690ff9e24b87e240
Vahe <vahehovakimyan574@gmail.com>
2023-08-21 18:10:56 +0400

    some changes

--- src/app/pages/Product/Card/CardNapkin/CardNapkinDisplay.jsx
+++ src/app/pages/Product/Card/CardNapkin/CardNapkinDisplay.jsx
@@ -15,79 +15,90 @@ export default function CardDisplay() {
 
 
     return (
-        <div className='product_card_page' >
 
-            <div className='product_card_page_img'>
-                <img src={sterile_image} alt="sterile_image" />
+        <div className='product_card_page_flex'>
+            <div className='product_card_page'>
 
-                <img src={medical_napkin} alt="medical_napkin_image" />
-            </div>
+                <div className='product_card_page_img'>
 
-            <div className='product_card_page_text_div'>
-                <h1>{product_napkins[key].name}</h1>
-
-                <div className='product_card_page_params'>
-                    <div className='product_parametrs'>
-                        <div className='product_size'>
-                            <div className='product_size_flex'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="13" viewBox="0 0 10 13" fill="none">
-                                    <path d="M0 1H9.50722" stroke="#12724F" stroke-width="0.462357" />
-                                    <path d="M0 12H9.50722" stroke="#12724F" stroke-width="0.462357" />
-                                    <path d="M4.10947 8.51486V4.48524H5.08042V8.51486H6.41548L4.59495 10.4825L2.77441 8.51486H4.10947ZM4.59495 2.51758L6.41548 4.48524H2.77441L4.59495 2.51758Z" fill="#12724F" />
-                                </svg>
-
-                                <span className='home_product_card_size_text'>{product_napkins[key].parametrs.size.from}</span>
-
-                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11" fill="none">
-                                    <path d="M0.253906 10.2539L0.253906 0.746683" stroke="#12724F" stroke-width="0.462357" />
-                                    <path d="M11.2539 10.2539L11.2539 0.746683" stroke="#12724F" stroke-width="0.462357" />
-                                    <path d="M7.76877 6.14468L3.73914 6.14468V5.17373H7.76877V3.83867L9.73642 5.6592L7.76877 7.47974V6.14468ZM1.77148 5.6592L3.73914 3.83867L3.73914 7.47974L1.77148 5.6592Z" fill="#12724F" />
-                                </svg>
-
-                                <span className='home_product_card_size_text'>{product_napkins[key].parametrs.size.to}</span>
-
-                            </div>
-                        </div>
-                        <div className='product_layer'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
-                                <g clip-path="url(#clip0_58_793)">
-                                    <path d="M13.5 7.35008L7.4 10.1601C7.26972 10.2195 7.12819 10.2503 6.985 10.2503C6.84181 10.2503 6.70028 10.2195 6.57 10.1601L0.5 7.35008M13.5 10.6001L7.4 13.4101C7.26972 13.4695 7.12819 13.5003 6.985 13.5003C6.84181 13.5003 6.70028 13.4695 6.57 13.4101L0.5 10.6001M7.47 6.90008C7.32168 6.96449 7.1617 6.99772 7 6.99772C6.8383 6.99772 6.67832 6.96449 6.53 6.90008L0.83 4.26008C0.737441 4.2134 0.659658 4.14194 0.605312 4.05366C0.550966 3.96538 0.52219 3.86375 0.52219 3.76008C0.52219 3.65642 0.550966 3.55479 0.605312 3.46651C0.659658 3.37823 0.737441 3.30677 0.83 3.26008L6.53 0.600083C6.67832 0.535676 6.8383 0.502441 7 0.502441C7.1617 0.502441 7.32168 0.535676 7.47 0.600083L13.17 3.24008C13.2626 3.28677 13.3403 3.35823 13.3947 3.44651C13.449 3.53479 13.4778 3.63642 13.4778 3.74008C13.4778 3.84375 13.449 3.94538 13.3947 4.03366C13.3403 4.12194 13.2626 4.1934 13.17 4.24008L7.47 6.90008Z" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
-                                </g>
-                                <defs>
-                                    <clipPath id="clip0_58_793">
-                                        <rect width="14" height="14" fill="white" />
-                                    </clipPath>
-                                </defs>
-                            </svg>
-                            <span className='home_product_card_size_text'>{product_napkins[key].parametrs.layer} շերտ</span>
-                        </div>
-                        <div className='product_count'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="13" viewBox="0 0 15 13" fill="none">
-                                <path d="M2 12H8C8.55228 12 9 11.5523 9 11V7C9 6.44772 8.55228 6 8 6H2C1.44772 6 1 6.44772 1 7V11C1 11.5523 1.44772 12 2 12Z" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M3.5 3.5H10.5C10.7652 3.5 11.0196 3.60536 11.2071 3.79289C11.3946 3.98043 11.5 4.23478 11.5 4.5V9.5" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M5.5 1H13C13.2652 1 13.5196 1.10536 13.7071 1.29289C13.8946 1.48043 14 1.73478 14 2V7.5" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-                            <span className='home_product_card_size_text'>{product_napkins[key].parametrs.count} հատ</span>
-                        </div>
+                    <div className='card_sterile_part'>
+                        {product_napkins[key].sterile ? <img src={sterile_image} alt="sterile_image" /> : <div className='sterile_img_for_null'></div>}
                     </div>
+
+                    <div className='home_product_card_img_div'>
+                        <img src={medical_napkin} alt="medical_napkin" className='home_product_card_img' />
+                    </div>
+
+
                 </div>
 
-                <p className='product_card_page_text'>
-                    Բինտեր, թանզիֆե մանրէազերծված բժշկական անձեռոցիկներ, մանրէազերծված և ոչ մանրէազերծված բժշկական վիրակապեր արտադրող;
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
-                    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                    It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
-                    with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of
-                    the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when
-                    an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
-                    but also the leap into electronic typesetting,
-                </p>
+                <div className='product_card_page_text_div'>
+                    <h1 className='product_card_page_title'>{product_napkins[key].name}</h1>
+
+                    <div className='product_card_page_params'>
+                        <div className='product_parametrs'>
+                            <div className='product_size'>
+                                <div className='product_size_flex'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="13" viewBox="0 0 10 13" fill="none">
+                                        <path d="M0 1H9.50722" stroke="#12724F" stroke-width="0.462357" />
+                                        <path d="M0 12H9.50722" stroke="#12724F" stroke-width="0.462357" />
+                                        <path d="M4.10947 8.51486V4.48524H5.08042V8.51486H6.41548L4.59495 10.4825L2.77441 8.51486H4.10947ZM4.59495 2.51758L6.41548 4.48524H2.77441L4.59495 2.51758Z" fill="#12724F" />
+                                    </svg>
+
+                                    <span className='home_product_card_size_text'>{product_napkins[key].parametrs.size.from}</span>
+
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="11" viewBox="0 0 12 11" fill="none">
+                                        <path d="M0.253906 10.2539L0.253906 0.746683" stroke="#12724F" stroke-width="0.462357" />
+                                        <path d="M11.2539 10.2539L11.2539 0.746683" stroke="#12724F" stroke-width="0.462357" />
+                                        <path d="M7.76877 6.14468L3.73914 6.14468V5.17373H7.76877V3.83867L9.73642 5.6592L7.76877 7.47974V6.14468ZM1.77148 5.6592L3.73914 3.83867L3.73914 7.47974L1.77148 5.6592Z" fill="#12724F" />
+                                    </svg>
+
+                                    <span className='home_product_card_size_text'>{product_napkins[key].parametrs.size.to}</span>
+
+                                </div>
+                            </div>
+                            <div className='product_layer'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                                    <g clip-path="url(#clip0_58_793)">
+                                        <path d="M13.5 7.35008L7.4 10.1601C7.26972 10.2195 7.12819 10.2503 6.985 10.2503C6.84181 10.2503 6.70028 10.2195 6.57 10.1601L0.5 7.35008M13.5 10.6001L7.4 13.4101C7.26972 13.4695 7.12819 13.5003 6.985 13.5003C6.84181 13.5003 6.70028 13.4695 6.57 13.4101L0.5 10.6001M7.47 6.90008C7.32168 6.96449 7.1617 6.99772 7 6.99772C6.8383 6.99772 6.67832 6.96449 6.53 6.90008L0.83 4.26008C0.737441 4.2134 0.659658 4.14194 0.605312 4.05366C0.550966 3.96538 0.52219 3.86375 0.52219 3.76008C0.52219 3.65642 0.550966 3.55479 0.605312 3.46651C0.659658 3.37823 0.737441 3.30677 0.83 3.26008L6.53 0.600083C6.67832 0.535676 6.8383 0.502441 7 0.502441C7.1617 0.502441 7.32168 0.535676 7.47 0.600083L13.17 3.24008C13.2626 3.28677 13.3403 3.35823 13.3947 3.44651C13.449 3.53479 13.4778 3.63642 13.4778 3.74008C13.4778 3.84375 13.449 3.94538 13.3947 4.03366C13.3403 4.12194 13.2626 4.1934 13.17 4.24008L7.47 6.90008Z" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_58_793">
+                                            <rect width="14" height="14" fill="white" />
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+                                <span className='home_product_card_size_text'>{product_napkins[key].parametrs.layer} շերտ</span>
+                            </div>
+                            <div className='product_count'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="15" height="13" viewBox="0 0 15 13" fill="none">
+                                    <path d="M2 12H8C8.55228 12 9 11.5523 9 11V7C9 6.44772 8.55228 6 8 6H2C1.44772 6 1 6.44772 1 7V11C1 11.5523 1.44772 12 2 12Z" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M3.5 3.5H10.5C10.7652 3.5 11.0196 3.60536 11.2071 3.79289C11.3946 3.98043 11.5 4.23478 11.5 4.5V9.5" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M5.5 1H13C13.2652 1 13.5196 1.10536 13.7071 1.29289C13.8946 1.48043 14 1.73478 14 2V7.5" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                                <span className='home_product_card_size_text'>{product_napkins[key].parametrs.count} հատ</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <p className='product_card_page_text'>
+                        Բինտեր, թանզիֆե մանրէազերծված բժշկական անձեռոցիկներ, մանրէազերծված և ոչ մանրէազերծված բժշկական վիրակապեր արտադրող;
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                        dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+                        with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of
+                        the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when
+                        an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                        but also the leap into electronic typesetting,
+                    </p>
+                </div>
+
+
+
+
             </div>
-
-
-
-
         </div>
+
     )
 }
